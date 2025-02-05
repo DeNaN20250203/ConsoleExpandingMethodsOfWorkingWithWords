f76c41f8bd45ccb78fbfcec75d42bf028f941baf
ДеЮра <doomov@yandex.ru>
2025-02-05 15:43:15 +0700

    Add files via upload

---
+++ ConsoleExpandingMethodsOfWorkingWithWords 001/Extesions.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ConsoleExpandingMethodsOfWorkingWithWords_001
+{
+	static class Extesions
+	{
+		/// <summary>
+		/// Метод, GetValueRefOrNullRef, позволяет обновлять значение по ключу, если оно существует:
+		/// </summary>
+		public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict,
+															TKey key,
+															TValue value)
+			where TKey : notnull
+		{
+			ref var dictVal = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+			if (!Unsafe.IsNullRef(ref dictVal))
+			{
+				dictVal = value;
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Метод CollectionsMarshal.GetValueRefOrAddDefault возвращает ссылку на значение, связанное с ключом, 
+		/// и логическое значение, указывающее, существует ли ключ. 
+		/// Поскольку метод возвращает ссылку, вы можете обновить значение:
+		/// </summary>
+		public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict,
+															TKey key,
+															TValue value)
+			where TKey : notnull
+		{
+			ref var dictVal = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out var exists);
+			if (exists)
+#pragma warning disable CS8603 // Возможно, возврат ссылки, допускающей значение NULL.
+				return dictVal;
+#pragma warning restore CS8603 // Возможно, возврат ссылки, допускающей значение NULL.
+			dictVal = value;
+			return value;
+		}
+	}
+}
---
+++ ConsoleExpandingMethodsOfWorkingWithWords 001/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleExpandingMethodsOfWorkingWithWords_001
+{
+	internal class Program
+    {
+        static void Main(string[] args)
+        {
+			var dict = new Dictionary<string, int>
+				{
+					{ "Яблоко", 1 },
+					{ "Банан", 2 }
+				};
+			{
+
+				// Пытаемся обновить значение по существующему ключу
+				bool updated = dict.TryUpdate("Яблоко", 10);
+				Console.WriteLine($"Обновлено 'Яблоко': {updated}, новое значение: {dict["Яблоко"]}");
+
+				// Пытаемся обновить значение по несуществующему ключу
+				updated = dict.TryUpdate("Апельсин", 3);
+				Console.WriteLine($"Обновлено 'Апельсин': {updated}");
+			}
+
+			{
+				// Получаем значение по существующему ключу
+				int appleValue = dict.GetOrAdd("Яблоко", 10);
+				Console.WriteLine($"Значение для 'Яблоко': {appleValue}");
+
+				// Добавляем новое значение по несуществующему ключу
+				int orangeValue = dict.GetOrAdd("Апельсин", 3);
+				Console.WriteLine($"Значение для 'Апельсин': {orangeValue}");
+
+				// Выводим все значения в словаре
+				foreach (var kvp in dict)
+				{
+					Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+				}
+			}
+			Console.ReadKey();
+        }
+    }
+}
